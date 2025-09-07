54442ffbd50ab746e7a8892508e9a770d0719203
Sheillah Timi <biyakisheila@gmail.com>
2025-09-07 15:18:27 +0300

    Fixed Context API implementation with proper structure

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,10 +1,6 @@
-import { useContext } from "react";
-import UserContext from "./UserContext";
 import UserInfo from "./UserInfo";
 
 function ProfilePage() {
-  const userData = useContext(UserContext);
-
   return <UserInfo />;
 }
 
