a93de53c8b7662ee87294a3c5a258556a784e646
Sheillah Timi <biyakisheila@gmail.com>
2025-09-07 15:08:51 +0300

    Added alx-react-app-props project refactored to use Context API

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
 import UserInfo from "./UserInfo";
 
 function ProfilePage() {
+  const userData = useContext(UserContext);
+
   return <UserInfo />;
 }
 
--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -5,7 +5,7 @@ function UserDetails() {
   const userData = useContext(UserContext);
 
   return (
-    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+    <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
